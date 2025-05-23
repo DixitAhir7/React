026d5e6b1bb4ff476f7a8d85a66fff7c7db11f33
dixit-ahir <dixitahir0050@gmail.com>
2025-05-23 22:10:49 +0530

    componentsbasics

--- 01-react_components/src/App.jsx
+++ 01-react_components/src/App.jsx
@@ -9,8 +9,26 @@ function UseWindowWidth() {
 
     return () => window.removeEventListener("resize", handleResize);
   });
-
   return width;
+}
+
+function AskUser() {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="enter name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Name name={name} />
+    </div>
+  );
+}
+
+function Name({ name }) {
+  return <p>Hello {name}</p>;
 }
 
 function App() {
@@ -18,4 +36,40 @@ function App() {
   return <div>Window width is {width}px</div>;
 }
 
-export { App, UseWindowWidth }
+function User() {
+  const [users] = useState([
+    { name: "Amit", age: 20, isOnline: true },
+    { name: "Ravi", age: 16, isOnline: false },
+    { name: "Kiran", age: 25, isOnline: true },
+    { name: "Pooja", age: 17, isOnline: false }
+  ]);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>User Status</h1>
+      {users.map((user, index) => (
+        <UserCard key={index} user={user} />
+      ))}
+    </div>
+  );
+}
+function UserCard({ user }) {
+  return (
+    <div style={{
+      border: "1px solid #ccc",
+      padding: "1rem",
+      borderRadius: "8px",
+      marginBottom: "1rem"
+    }}>
+      <h3>{user.name}</h3>
+      {user.isOnline ? (
+        <p style={{ color: "green" }}>🟢 Online</p>
+      ) : (
+        <p style={{ color: "gray" }}>⚫ Offline</p>
+      )}
+    </div>
+  );
+}
+
+
+export { App, UseWindowWidth, Name, AskUser, UserCard, User }
--- 01-react_components/src/main.jsx
+++ 01-react_components/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App, UseWindowWidth } from './App.jsx'
+import { App, UseWindowWidth, Name, AskUser, UserCard, User } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <useWindowWidth />
+    <UseWindowWidth />
+    <Name />
+    <AskUser />
+    <UserCard />
+    <User />
   </StrictMode>,
 )
