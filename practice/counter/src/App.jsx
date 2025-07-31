1c744a7aeefe7d0ebb81186fd6571aba02376f72
Dixit_ahir7 <dixitahir0050@gmail.com>
2025-07-31 13:46:35 +0530

    useReducer

--- practice/counter/src/App.jsx
+++ practice/counter/src/App.jsx
@@ -1,14 +1,10 @@
 import Counter from './components/Counter';
-import Form from './components/Form';
-import Toggle from './components/Toggle';
 import './index.css'
 
 function App() {
   return (
     <>
       <Counter />
-      {/* <Toggle /> */}
-      <Form />
     </>
   );
 }
